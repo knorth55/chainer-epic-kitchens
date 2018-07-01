6b729b7c3143dd460a19f43790abf432fb928e75
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-07-01 20:54:52 +0900

    add train_multi for ssd objectness

---
+++ examples/ssd_objectness/train_multi.py
@@ -0,0 +1,193 @@
+import argparse
+import copy
+import multiprocessing
+import numpy as np
+
+import chainer
+from chainer.optimizer_hooks import WeightDecay
+from chainer import serializers
+from chainer import training
+from chainer.training import extensions
+from chainer.training import triggers
+from chainercv.chainer_experimental.datasets.sliceable import TransformDataset
+from chainercv.links.model.ssd import GradientScaling
+from chainercv.links.model.ssd import multibox_loss
+from chainercv.links.model.ssd import random_crop_with_bbox_constraints
+from chainercv.links.model.ssd import random_distort
+from chainercv.links.model.ssd import resize_with_random_interpolation
+from chainercv.links import SSD300
+from chainercv.links import SSD512
+from chainercv import transforms
+import chainermn
+
+from chainer_epic_kitchens.datasets import EpicKitchensBboxDataset
+
+
+class Transform(object):
+
+    def __init__(self, coder, size, mean):
+        # to send cpu, make a copy
+        self.coder = copy.copy(coder)
+        self.coder.to_cpu()
+
+        self.size = size
+        self.mean = mean
+
+    def __call__(self, in_data):
+        # There are five data augmentation steps
+        # 1. Color augmentation
+        # 2. Random expansion
+        # 3. Random cropping
+        # 4. Resizing with random interpolation
+        # 5. Random horizontal flipping
+
+        img, bbox, label = in_data
+
+        # all label is only (bg, fg).
+        label = np.zeros(label.shape)
+
+        # 1. Color augmentation
+        img = random_distort(img)
+
+        # 2. Random expansion
+        if np.random.randint(2):
+            img, param = transforms.random_expand(
+                img, fill=self.mean, return_param=True)
+            bbox = transforms.translate_bbox(
+                bbox, y_offset=param['y_offset'], x_offset=param['x_offset'])
+
+        # 3. Random cropping
+        img, param = random_crop_with_bbox_constraints(
+            img, bbox, return_param=True)
+        bbox, param = transforms.crop_bbox(
+            bbox, y_slice=param['y_slice'], x_slice=param['x_slice'],
+            allow_outside_center=False, return_param=True)
+        label = label[param['index']]
+
+        # 4. Resizing with random interpolatation
+        _, H, W = img.shape
+        img = resize_with_random_interpolation(img, (self.size, self.size))
+        bbox = transforms.resize_bbox(bbox, (H, W), (self.size, self.size))
+
+        # 5. Random horizontal flipping
+        img, params = transforms.random_flip(
+            img, x_random=True, return_param=True)
+        bbox = transforms.flip_bbox(
+            bbox, (self.size, self.size), x_flip=params['x_flip'])
+
+        # Preparation for SSD network
+        img -= self.mean
+        mb_loc, mb_label = self.coder.encode(bbox, label)
+
+        return img, mb_loc, mb_label
+
+
+class MultiboxTrainChain(chainer.Chain):
+
+    def __init__(self, model, alpha=1, k=3, comm=None):
+        super(MultiboxTrainChain, self).__init__()
+        with self.init_scope():
+            self.model = model
+        self.alpha = alpha
+        self.k = k
+        self.comm = comm
+
+    def __call__(self, imgs, gt_mb_locs, gt_mb_labels):
+        mb_locs, mb_confs = self.model(imgs)
+        loc_loss, conf_loss = multibox_loss(
+            mb_locs, mb_confs, gt_mb_locs, gt_mb_labels, self.k, self.comm)
+        loss = loc_loss * self.alpha + conf_loss
+
+        chainer.reporter.report(
+            {'loss': loss, 'loss/loc': loc_loss, 'loss/conf': conf_loss},
+            self)
+
+        return loss
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model', choices=('ssd300', 'ssd512'), default='ssd300')
+    parser.add_argument('--batchsize', type=int, default=32)
+    parser.add_argument('--out', default='result')
+    parser.add_argument('--resume')
+    args = parser.parse_args()
+
+    comm = chainermn.create_communicator()
+    device = comm.intra_rank
+
+    if args.model == 'ssd300':
+        model = SSD300(
+            n_fg_class=1,
+            pretrained_model='imagenet')
+    elif args.model == 'ssd512':
+        model = SSD512(
+            n_fg_class=1,
+            pretrained_model='imagenet')
+
+    model.use_preset('evaluate')
+    train_chain = MultiboxTrainChain(model)
+    chainer.cuda.get_device_from_id(device).use()
+    model.to_gpu()
+
+    train = EpicKitchensBboxDataset(year='2018', split='train')
+    if comm.rank == 0:
+        indices = np.arange(len(train))
+    else:
+        indices = None
+    train = TransformDataset(
+        train, ('img', 'mb_loc', 'mb_label'),
+        Transform(model.coder, model.insize, model.mean))
+
+    indices = chainermn.scatter_dataset(indices, comm, shuffle=True)
+    train = train.slice[indices]
+
+    # http://chainermn.readthedocs.io/en/latest/tutorial/tips_faqs.html#using-multiprocessiterator
+    if hasattr(multiprocessing, 'set_start_method'):
+        multiprocessing.set_start_method('forkserver')
+    train_iter = chainer.iterators.MultiprocessIterator(
+        train, args.batchsize, n_processes=2)
+
+    # initial lr is set to 1e-3 by ExponentialShift
+    optimizer = chainermn.create_multi_node_optimizer(
+        chainer.optimizers.MomentumSGD(), comm)
+    optimizer.setup(train_chain)
+    for param in train_chain.params():
+        if param.name == 'b':
+            param.update_rule.add_hook(GradientScaling(2))
+        else:
+            param.update_rule.add_hook(WeightDecay(0.0005))
+
+    updater = training.updaters.StandardUpdater(
+        train_iter, optimizer, device=device)
+    trainer = training.Trainer(updater, (12, 'epoch'), args.out)
+    trainer.extend(
+        extensions.ExponentialShift('lr', 0.1, init=1e-3),
+        trigger=triggers.ManualScheduleTrigger([8, 10], 'epoch'))
+
+    if comm.rank == 0:
+        log_interval = 10, 'iteration'
+        trainer.extend(extensions.LogReport(
+            log_name='log.json', trigger=log_interval))
+        trainer.extend(extensions.observe_lr(), trigger=log_interval)
+        trainer.extend(
+            extensions.PrintReport(
+                ['epoch', 'iteration', 'lr',
+                 'main/loss', 'main/loss/loc', 'main/loss/conf']),
+            trigger=log_interval)
+        trainer.extend(extensions.ProgressBar(update_interval=1))
+
+        trainer.extend(
+            extensions.snapshot_object(
+                model, 'model_iter_{.updater.iteration}.npz'),
+            trigger=(1, 'epoch'))
+
+    if args.resume:
+        serializers.load_npz(args.resume, trainer)
+
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
