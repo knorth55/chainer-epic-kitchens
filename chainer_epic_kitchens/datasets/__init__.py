f49fcafe991af8304cd53f1b4c5114b9f74be05b
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-07-06 23:24:52 +0900

    add epic_kitchens_bbox_category_names

--- chainer_epic_kitchens/datasets/__init__.py
+++ chainer_epic_kitchens/datasets/__init__.py
@@ -1,4 +1,7 @@
 from chainer_epic_kitchens.datasets import epic_kitchens_bbox_dataset  # NOQA
 from chainer_epic_kitchens.datasets.epic_kitchens_bbox_dataset import EpicKitchensBboxDataset  # NOQA
 from chainer_epic_kitchens.datasets import epic_kitchens_utils  # NOQA
+from chainer_epic_kitchens.datasets.epic_kitchens_utils import epic_kitchens_bbox_category_names  # NOQA
 from chainer_epic_kitchens.datasets.epic_kitchens_utils import epic_kitchens_bbox_label_names  # NOQA
+from chainer_epic_kitchens.datasets.epic_kitchens_utils import get_epic_kitchens  # NOQA
+from chainer_epic_kitchens.datasets.epic_kitchens_utils import get_epic_kitchens_category  # NOQA
